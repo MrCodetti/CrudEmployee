c9728b689474a37ed452bdecdbe679b7463ecd54
Bartosz Porada <bporada@outlook.com>
2020-05-05 13:29:46 +0200

    Projektdateien hinzufügen.

---
+++ CrudEmployee/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace CrudEmployee
+{
+    partial class frmEmployee
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ssInfo = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.dgvEmployee = new System.Windows.Forms.DataGridView();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblCity = new System.Windows.Forms.Label();
+            this.lblDepartment = new System.Windows.Forms.Label();
+            this.lblGender = new System.Windows.Forms.Label();
+            this.txbName = new System.Windows.Forms.TextBox();
+            this.txbCity = new System.Windows.Forms.TextBox();
+            this.txbDepartment = new System.Windows.Forms.TextBox();
+            this.cobGender = new System.Windows.Forms.ComboBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.ssInfo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEmployee)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ssInfo
+            // 
+            this.ssInfo.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.ssInfo.Location = new System.Drawing.Point(0, 508);
+            this.ssInfo.Name = "ssInfo";
+            this.ssInfo.Size = new System.Drawing.Size(513, 22);
+            this.ssInfo.TabIndex = 0;
+            this.ssInfo.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(309, 17);
+            this.toolStripStatusLabel1.Text = "Note: Double click on a row to view, edit or delete record.";
+            // 
+            // dgvEmployee
+            // 
+            this.dgvEmployee.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvEmployee.Location = new System.Drawing.Point(12, 234);
+            this.dgvEmployee.Name = "dgvEmployee";
+            this.dgvEmployee.RowHeadersVisible = false;
+            this.dgvEmployee.Size = new System.Drawing.Size(489, 262);
+            this.dgvEmployee.TabIndex = 1;
+            this.dgvEmployee.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvEmployee_CellDoubleClick);
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(54, 27);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(125, 20);
+            this.lblName.TabIndex = 2;
+            this.lblName.Text = "Employee Name";
+            // 
+            // lblCity
+            // 
+            this.lblCity.AutoSize = true;
+            this.lblCity.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCity.Location = new System.Drawing.Point(54, 62);
+            this.lblCity.Name = "lblCity";
+            this.lblCity.Size = new System.Drawing.Size(92, 20);
+            this.lblCity.TabIndex = 3;
+            this.lblCity.Text = "Current City";
+            // 
+            // lblDepartment
+            // 
+            this.lblDepartment.AutoSize = true;
+            this.lblDepartment.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDepartment.Location = new System.Drawing.Point(54, 98);
+            this.lblDepartment.Name = "lblDepartment";
+            this.lblDepartment.Size = new System.Drawing.Size(94, 20);
+            this.lblDepartment.TabIndex = 4;
+            this.lblDepartment.Text = "Department";
+            // 
+            // lblGender
+            // 
+            this.lblGender.AutoSize = true;
+            this.lblGender.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGender.Location = new System.Drawing.Point(54, 139);
+            this.lblGender.Name = "lblGender";
+            this.lblGender.Size = new System.Drawing.Size(63, 20);
+            this.lblGender.TabIndex = 5;
+            this.lblGender.Text = "Gender";
+            // 
+            // txbName
+            // 
+            this.txbName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbName.Location = new System.Drawing.Point(207, 24);
+            this.txbName.Name = "txbName";
+            this.txbName.Size = new System.Drawing.Size(255, 26);
+            this.txbName.TabIndex = 6;
+            // 
+            // txbCity
+            // 
+            this.txbCity.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbCity.Location = new System.Drawing.Point(207, 59);
+            this.txbCity.Name = "txbCity";
+            this.txbCity.Size = new System.Drawing.Size(255, 26);
+            this.txbCity.TabIndex = 7;
+            // 
+            // txbDepartment
+            // 
+            this.txbDepartment.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbDepartment.Location = new System.Drawing.Point(207, 95);
+            this.txbDepartment.Name = "txbDepartment";
+            this.txbDepartment.Size = new System.Drawing.Size(255, 26);
+            this.txbDepartment.TabIndex = 8;
+            // 
+            // cobGender
+            // 
+            this.cobGender.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cobGender.FormattingEnabled = true;
+            this.cobGender.Items.AddRange(new object[] {
+            "FEMALE",
+            "MALE"});
+            this.cobGender.Location = new System.Drawing.Point(207, 131);
+            this.cobGender.Name = "cobGender";
+            this.cobGender.Size = new System.Drawing.Size(255, 28);
+            this.cobGender.TabIndex = 9;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(85, 180);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(94, 32);
+            this.btnSave.TabIndex = 10;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(207, 180);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(94, 32);
+            this.btnClear.TabIndex = 11;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(328, 180);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(94, 32);
+            this.btnDelete.TabIndex = 12;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // frmEmployee
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(513, 530);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.cobGender);
+            this.Controls.Add(this.txbDepartment);
+            this.Controls.Add(this.txbCity);
+            this.Controls.Add(this.txbName);
+            this.Controls.Add(this.lblGender);
+            this.Controls.Add(this.lblDepartment);
+            this.Controls.Add(this.lblCity);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.dgvEmployee);
+            this.Controls.Add(this.ssInfo);
+            this.MaximizeBox = false;
+            this.Name = "frmEmployee";
+            this.ShowIcon = false;
+            this.Text = "Employee Management";
+            this.Load += new System.EventHandler(this.frmEmployee_Load);
+            this.ssInfo.ResumeLayout(false);
+            this.ssInfo.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEmployee)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip ssInfo;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.DataGridView dgvEmployee;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblCity;
+        private System.Windows.Forms.Label lblDepartment;
+        private System.Windows.Forms.Label lblGender;
+        private System.Windows.Forms.TextBox txbName;
+        private System.Windows.Forms.TextBox txbCity;
+        private System.Windows.Forms.TextBox txbDepartment;
+        private System.Windows.Forms.ComboBox cobGender;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
+
---
+++ CrudEmployee/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace CrudEmployee
+{
+    public partial class frmEmployee : Form
+    {
+        string connString = (@"Data Source=DESKTOP-0S31MOV\SQLKURS;" +
+                             "Initial Catalog=dbKundenVerwaltung;Integrated Security=sspi");
+
+        SqlConnection sqlCon;
+        SqlCommand sqlCmd;
+        string EmployeeId = "";
+
+        public frmEmployee()
+        {
+            InitializeComponent();
+            sqlCon = new SqlConnection(connString);
+            sqlCon.Open();
+        }
+
+        private void frmEmployee_Load(object sender, EventArgs e)
+        {
+            //dgvEmployee.AutoGenerateColumns = false;
+            dgvEmployee.DataSource = FetchEmpDetails();
+        }
+
+        private DataTable FetchEmpDetails()
+        {
+            if (sqlCon.State == ConnectionState.Closed)
+            {
+                sqlCon.Open();
+            }
+            DataTable dtData = new DataTable();
+            sqlCmd = new SqlCommand("uspEployee", sqlCon);
+            sqlCmd.CommandType = CommandType.StoredProcedure;
+            sqlCmd.Parameters.AddWithValue("@ActionType", "FetchData");
+            SqlDataAdapter sqlSda = new SqlDataAdapter(sqlCmd);
+            sqlSda.Fill(dtData);
+            return dtData;
+        }
+
+        private DataTable FetchEmpRecords(string empId)
+        {
+            if (sqlCon.State == ConnectionState.Closed)
+            {
+                sqlCon.Open();
+            }
+            DataTable sqlDt = new DataTable();
+            sqlCmd = new SqlCommand("uspEployee", sqlCon);
+            sqlCmd.CommandType = CommandType.StoredProcedure;
+            sqlCmd.Parameters.AddWithValue("@ActionType", "FetchRecord");
+            sqlCmd.Parameters.AddWithValue("@EmpId", empId);
+            SqlDataAdapter sqlDa = new SqlDataAdapter(sqlCmd);
+            sqlDa.Fill(sqlDt);
+            return sqlDt;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (txbName.Text == "" && 
+                txbCity.Text == "" && 
+                txbDepartment.Text == "" &&
+                cobGender.SelectedIndex <= -1)
+            {
+                MessageBox.Show("Bitte Keine freie felder!", "Syntax Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                cobGender.Select();
+            }
+            else
+            {
+                try
+                {
+                    if (sqlCon.State == ConnectionState.Closed)
+                    {
+                        sqlCon.Open();
+                    }
+                    sqlCmd = new SqlCommand("uspEployee", sqlCon);
+                    sqlCmd.CommandType = CommandType.StoredProcedure;
+                    sqlCmd.Parameters.AddWithValue("@ActionType", "SaveData");
+                    sqlCmd.Parameters.AddWithValue("@EmpId", EmployeeId);
+                    sqlCmd.Parameters.AddWithValue("@Name", txbName.Text);
+                    sqlCmd.Parameters.AddWithValue("@City", txbCity.Text);
+                    sqlCmd.Parameters.AddWithValue("@Deparment", txbDepartment.Text);
+                    sqlCmd.Parameters.AddWithValue("@Gender", cobGender.Text);
+                    int numRes = sqlCmd.ExecuteNonQuery();
+                    if (numRes > 0)
+                    {
+                        MessageBox.Show("Record Saved Successfully !!!");
+                        ClearAllData();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Try Again !!!");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            ClearAllData();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (EmployeeId != "")
+            {
+                try
+                {
+                    if (sqlCon.State == ConnectionState.Closed)
+                    {
+                        sqlCon.Open();
+                    }
+                    DataTable sqlDt = new DataTable();
+                    sqlCmd = new SqlCommand("uspEployee", sqlCon);
+                    sqlCmd.CommandType = CommandType.StoredProcedure;
+                    sqlCmd.Parameters.AddWithValue("@ActionType", "DeleteData");
+                    sqlCmd.Parameters.AddWithValue("@EmpId", EmployeeId);
+                    int numRes = sqlCmd.ExecuteNonQuery();
+                    if (numRes > 0)
+                    {
+                        MessageBox.Show("Record Deleted Successfully !!!");
+                        ClearAllData();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Try Again !!!");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a record !!!");
+            }
+        }
+
+        private void dgvEmployee_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                btnSave.Text = "Update";
+                EmployeeId = dgvEmployee.Rows[e.RowIndex].Cells[0].Value.ToString();
+                DataTable DT = FetchEmpRecords(EmployeeId);
+                if (DT.Rows.Count > 0)
+                {
+                    txbName.Text = DT.Rows[0][1].ToString();
+                    txbCity.Text = DT.Rows[0][2].ToString();
+                    txbDepartment.Text = DT.Rows[0][3].ToString();
+                    cobGender.Text = DT.Rows[0][4].ToString();
+                }
+                else
+                {
+                    ClearAllData();
+                }
+            }
+        }
+
+        private void ClearAllData()
+        {
+            btnSave.Text = "Save";
+            txbName.Clear();
+            txbCity.Clear();
+            txbDepartment.Clear();
+            cobGender.SelectedIndex = -1;
+            EmployeeId = "";
+            dgvEmployee.AutoGenerateColumns = false;
+            dgvEmployee.DataSource = FetchEmpDetails();
+        }
+    }
+}
